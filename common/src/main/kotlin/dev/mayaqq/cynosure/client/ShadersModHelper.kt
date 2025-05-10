a56ea98488bb9d4446d43c0fdcf6b8d505f08d54
sery <sery@sappho.company>
2025-05-10 14:55:23 +0200

    use lmf

--- common/src/main/kotlin/dev/mayaqq/cynosure/client/ShadersModHelper.kt
+++ common/src/main/kotlin/dev/mayaqq/cynosure/client/ShadersModHelper.kt
@@ -2,6 +2,9 @@ package dev.mayaqq.cynosure.client
 
 import dev.mayaqq.cynosure.utils.isModLoaded
 import net.irisshaders.iris.api.v0.IrisApi
+import java.lang.invoke.LambdaMetafactory
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType
 import java.util.function.BooleanSupplier
 
 public val isShaderPackInUse: Boolean
@@ -13,9 +16,12 @@ public val isShaderPackInUse: Boolean
 private var shaderPackInUseSupplier: BooleanSupplier? = null
 
 private fun init(): BooleanSupplier = when {
+    // If Iris or Oculus is loaded, use their api
     isModLoaded("iris") || isModLoaded("oculus") ->
         BooleanSupplier { IrisApi.getInstance().isShaderPackInUse }
-    Package.getPackage("net.optifine") != null -> optifineShaderInUse()
+    // If optifine is loaded, create a scary reflective accessor to its internals
+    Package.getPackage("net.optifine.shaders") != null -> optifineShaderInUse()
+    // Otherwise just always return false
     else -> BooleanSupplier { false }
 }
 
@@ -24,13 +30,17 @@ private fun optifineShaderInUse(): BooleanSupplier {
         val ofShaders = Class.forName("net.optifine.shaders.Shaders")
         val field = ofShaders.getDeclaredField("shaderPackLoaded")
         field.isAccessible = true
-        return BooleanSupplier {
-            try {
-                field.getBoolean(null)
-            } catch (ignored: IllegalAccessException) {
-                false
-            }
-        }
+
+        val lookup = MethodHandles.lookup()
+        return LambdaMetafactory.metafactory(
+            lookup,
+            "getAsBoolean",
+            MethodType.methodType(BooleanSupplier::class.java),
+            MethodType.methodType(Boolean::class.javaPrimitiveType),
+            lookup.unreflectGetter(field),
+            MethodType.methodType(Boolean::class.javaPrimitiveType)
+        ).target.invoke() as BooleanSupplier
+
     } catch (ignored: Exception) {
         return BooleanSupplier { false }
     }
